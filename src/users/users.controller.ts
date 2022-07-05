895ebd31d53b2bc6ee3434ea68785f5bdef5fe1a
omarZuluaga <omarzuluaga123@hotmail.com>
2022-07-05 09:28:00 -0500

    interceptor to find the current user added (we use an interceptor in the user controller to management the current user)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,13 +8,16 @@ import { Controller,
   Delete, 
   Patch, 
   NotFoundException,
-  Session} from '@nestjs/common';
+  Session,
+  UseInterceptors} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto'; 
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
  
 @Controller('auth')
@@ -25,9 +28,14 @@ export class UsersController {
               private authService: AuthService) {}
 
   
+  // @Get('/whoami')
+  // whoAmI(@Session() session:any) {
+  //   return this.userService.findOne(session.userId);
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session:any) {
-    return this.userService.findOne(session.userId);
+  whoAmI(@CurrentUser() user:string ){ 
+    return user;
   }
 
   @Post('/signout')
